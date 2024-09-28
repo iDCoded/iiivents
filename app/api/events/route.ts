e64a9a60af6be16280ee922e117b4f43c83619ab
Dhruv Anand <ananddhruv29@gmail.com>
2024-09-28 23:37:48 +0530

    Implemented `POST` method

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,6 @@
 import dbConnect from "@/lib/dbConnect";
 import Event from "@/models/Event";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
 	await dbConnect();
@@ -11,5 +11,30 @@ export async function GET() {
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	} catch (err: any) {
 		return NextResponse.json({ err: err.message });
+	}
+}
+
+export async function POST(req: NextRequest) {
+	await dbConnect();
+	try {
+		const body = await req.json();
+
+		// const { eventName, eventDescription, eventPrice, eventDate } = body;
+
+		const newEvent = new Event({
+			eventName: body.eventName,
+			eventDescription: body.eventDescription,
+			eventPrice: body.eventPrice,
+			eventDate: body.eventDate,
+		});
+
+		await newEvent.save();
+
+		return NextResponse.json({
+			message: "Event created successfully",
+			event: newEvent,
+		});
+	} catch (err) {
+		console.log(err.message);
 	}
 }
