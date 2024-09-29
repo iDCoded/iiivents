dcc999f6938f27dc9ec5bc59f78c9fba0721d904
Dhruv Anand <ananddhruv29@gmail.com>
2024-09-30 03:19:47 +0530

    Fixed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
 	return (
 		<>
-			<footer className="bg-white absolute left-0 bottom-0 w-full h-12">
+			<footer className=" absolute left-0 bottom-0 w-full h-12">
 				<div className="flex flex-row justify-center items-center h-full">
 					<span className="text-sm font-semibold text-slate-500">
 						iiivents | Made by Dhruv
