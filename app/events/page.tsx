f2e05821ada88f430f8148b688a8e452e7f5050b
Dhruv Anand <ananddhruv29@gmail.com>
2024-10-02 00:15:37 +0530

    Removed `/events` page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,30 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import EventCard from "@/components/EventCard";
-
 export default async function Page() {
-	const fetchEvents = async () => {
-		const res = await fetch("/api/events", {
-			cache: "no-store",
-		});
-		const events = await res.json();
-		return events;
-	};
-	const events = await fetchEvents();
-
-	return (
-		<>
-			<div>
-				{events.map((event: any) => {
-					return (
-						<EventCard
-							key={event.eventName}
-							eventName={event.eventName}
-							eventDescription={event.eventDescription}
-							eventPrice={event.eventPrice}
-						/>
-					);
-				})}
-			</div>
-		</>
-	);
+	return <></>;
 }
