a2a4b82b4869fcaed2d1407b8203e9d3bd60d756
Dhruv Anand <ananddhruv29@gmail.com>
2024-10-01 22:17:04 +0530

    Updated Footer content
    Added tech stack to the footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,10 +3,13 @@ import React from "react";
 const Footer = () => {
 	return (
 		<>
-			<footer className="bg-white fixed left-0 bottom-0 w-full h-12">
-				<div className="flex flex-row justify-center items-center h-full">
+			<footer className="bg-white  w-full h-12">
+				<div className="flex flex-col justify-center items-center h-full">
 					<span className="text-sm font-semibold text-slate-500">
 						iiivents | Made by Dhruv
+					</span>
+					<span className="text-xs font-semibold text-slate-500">
+						Built with Next.js & shadcn
 					</span>
 				</div>
 			</footer>
